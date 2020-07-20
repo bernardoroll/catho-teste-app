8e8b95183177f2bf32933078773ce9b02ad08c4b
Bernardo Salgueiro <bernardo.roll@gmail.com>
2020-07-20 00:33:34 -0300

    Add binding adapter to show or hide a View

--- app/src/main/java/br/com/bernardoroll/catho/adapter/BindingAdapters.kt
+++ app/src/main/java/br/com/bernardoroll/catho/adapter/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package br.com.bernardoroll.catho.adapter
 
 import android.graphics.drawable.Drawable
+import android.view.View
 import androidx.databinding.BindingAdapter
 import com.mikhaellopez.circularimageview.CircularImageView
 
@@ -9,4 +10,9 @@ fun CircularImageView.setCircularImageDrawable(drawable: Drawable?) {
     drawable?.let {
         this.setImageDrawable(it)
     }
+}
+
+@BindingAdapter("present")
+fun View.setVisibility(present: Boolean? = false) {
+    this.visibility = if (present == true) View.VISIBLE else View.GONE
 }
